0d7102fed4cb3c68dc4c343c504bbe0b09431ff9
Joel Miguel <joel.miguel@utec.edu.pe>
2024-10-07 13:30:21 -0500

    acabado

--- app/main.py
+++ app/main.py
@@ -4,16 +4,20 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-app.include_router(reservation_router, prefix="/api/v1")
-
-@app.get("/")
-def read_root():
-    return {"message": "Welcome to the Reservations microservice!"}
+origins = [
+    "http://localhost:5173",  # URL del frontend
+]
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,  # Permitir estos orígenes
     allow_credentials=True,
-    allow_methods=[""],  # Permitir todos los métodos (GET, POST, etc.)
-    allow_headers=[""],  # Permitir todas las cabeceras
+    allow_methods=["*"],  # Permitir todos los métodos (GET, POST, etc.)
+    allow_headers=["*"],  # Permitir todas las cabeceras
 )
+
+app.include_router(reservation_router, prefix="/api/v1")
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the Reservations microservice!"}
